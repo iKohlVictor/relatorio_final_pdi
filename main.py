738727ae76dfc0ee2e14a1a03a2e9fe2c54241db
Ian Kohl <ianvictor181099@gmail.com>
2023-07-10 20:58:35 -0300

    finalizando o trabalho

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 # funções
 
@@ -59,7 +60,8 @@ def load_histograma(histograma, nomeDaJanela, xLabel, yLabel):
     plt.title(nomeDaJanela)
     plt.xlabel(xLabel)
     plt.ylabel(yLabel)
-    plt.savefig('./imagem_convertida/01/01-histograma_original.png')
+    plt.savefig('./imagem_convertida/' + pasta + '/' +
+                arquivo + '/01-histograma_original.png')
 
 
 def limiarizar(imagem, limiar, limite_maximo, tipo):
@@ -70,7 +72,15 @@ def limiarizar(imagem, limiar, limite_maximo, tipo):
 
 
 # programa principal
+pasta = "LuzBranca_Baixo"
+arquivo = "C.01_T.1_P.0001"
+
 nomeDaImagem = "C.01_T.1_P.0001.jpg"
+
+
+diretorioDestino = './imagem_convertida/' + pasta + '/' + arquivo
+if not os.path.exists(diretorioDestino):
+    os.makedirs(diretorioDestino)
 
 # Carrega a imagem
 
@@ -78,7 +88,8 @@ caminhoDaImagem = ("./LuzBranca_Baixo/" + nomeDaImagem)
 
 
 imagem = cv2.imread(caminhoDaImagem)
-cv2.imwrite("./imagem_convertida/01/01-imagem_original.jpg", imagem)
+cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+            arquivo+"/01-imagem_original.jpg", imagem)
 
 histograma_original = cv2.calcHist([imagem], [0], None, [256], [0, 256])
 load_histograma(histograma_original, "Histograma Original",
@@ -87,7 +98,7 @@ load_histograma(histograma_original, "Histograma Original",
 imagem_to_yuv = cv2.cvtColor(imagem, cv2.COLOR_BGR2YUV)
 imagem_to_yuv[:, :, 0] = cv2.equalizeHist(imagem_to_yuv[:, :, 0])
 imagem_equalizada = cv2.cvtColor(imagem_to_yuv, cv2.COLOR_YUV2BGR)
-cv2.imwrite("./imagem_convertida/01/01-imagem_equalizada.jpg",
+cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/01-imagem_equalizada.jpg",
             imagem_equalizada)
 
 histograma_equalizado = cv2.calcHist(
@@ -99,31 +110,41 @@ load_histograma(histograma_equalizado, "Histograma Original e Histograma Equaliz
 # separa os canais de cores
 blue_channel, green_channel, red_channel = get_rgb_channels(imagem)
 
-cv2.imwrite("./imagem_convertida/01/02-imagem_r.jpg", red_channel)
-cv2.imwrite("./imagem_convertida/01/03-imagem_g.jpg", green_channel)
-cv2.imwrite("./imagem_convertida/01/04-imagem_b.jpg", red_channel)
+cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+            arquivo+"/02-imagem_r.jpg", red_channel)
+cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+            arquivo+"/03-imagem_g.jpg", green_channel)
+cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+            arquivo+"/04-imagem_b.jpg", red_channel)
 
 
 imagem_hsv, hue_channel, saturation_channel, value_channel = get_hsv_channels(
     imagem)
 
-cv2.imwrite("./imagem_convertida/01/05-imagem_hsv.jpg", imagem_hsv)
-cv2.imwrite("./imagem_convertida/01/06-imagem_h.jpg", hue_channel)
-cv2.imwrite("./imagem_convertida/01/07-imagem_s.jpg", saturation_channel)
-cv2.imwrite("./imagem_convertida/01/08-imagem_v.jpg", value_channel)
+cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+            arquivo+"/05-imagem_hsv.jpg", imagem_hsv)
+cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+            arquivo+"/06-imagem_h.jpg", hue_channel)
+cv2.imwrite("./imagem_convertida/"+pasta + '/' +
+            arquivo +
+            "/07-imagem_s.jpg", saturation_channel)
+cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+            arquivo+"/08-imagem_v.jpg", value_channel)
 
 
 # aplicando o filtro
 
 imagem_filtrada = cv2.GaussianBlur(value_channel, (5, 5), 0)
 
-cv2.imwrite("./imagem_convertida/01/09-imagem_filtrada.jpg", imagem_filtrada)
+cv2.imwrite("./imagem_convertida/"+pasta + '/' +
+            arquivo +
+            "/09-imagem_filtrada.jpg", imagem_filtrada)
 
 # limiarização
 
 imagem_limiarizada = limiarizar(imagem_filtrada, 127, 255, cv2.THRESH_BINARY)
 
-cv2.imwrite("./imagem_convertida/01/10-imagem_limiarizada.jpg",
+cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/10-imagem_limiarizada.jpg",
             imagem_limiarizada)
 
 # morfológicas
@@ -135,13 +156,15 @@ imagem_processada = cv2.morphologyEx(
 imagem_processada = cv2.morphologyEx(
     imagem_processada, cv2.MORPH_CLOSE, kernel, iterations=4)
 
-cv2.imwrite("./imagem_convertida/01/11-imagem_dilatada.jpg", imagem_processada)
+cv2.imwrite("./imagem_convertida/"+pasta + '/' +
+            arquivo +
+            "/11-imagem_dilatada.jpg", imagem_processada)
 
 # multiplicar
 
 imagem_multiplicada = np.multiply(imagem, imagem_processada)
 
-cv2.imwrite("./imagem_convertida/01/12-imagem_multiplicada.jpg",
+cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/12-imagem_multiplicada.jpg",
             imagem_multiplicada)
 
 
@@ -165,7 +188,7 @@ y = cy - 300 // 2
 
 retangulo_recortado = imagem[max(y, 0):y + 300, max(x, 0):x + 300]
 
-cv2.imwrite("./imagem_convertida/01/13-retangulo_recortado.jpg",
+cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/13-retangulo_recortado.jpg",
             retangulo_recortado)
 
 variancia = np.var(retangulo_recortado)
