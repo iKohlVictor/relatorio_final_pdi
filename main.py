860871152ca9876050f419cfb10b61b3fbe9a3d1
Ian Kohl <ianvictor181099@gmail.com>
2023-07-10 21:29:06 -0300

    preguica move montanhas

--- main.py
+++ main.py
@@ -71,132 +71,138 @@ def limiarizar(imagem, limiar, limite_maximo, tipo):
     return imagem_limiarizada
 
 
-# programa principal
-pasta = "LuzBranca_Baixo"
-arquivo = "C.01_T.1_P.0001"
+nome_arquivos = []
 
-nomeDaImagem = "C.01_T.1_P.0001.jpg"
+for nome_arquivo in os.listdir("./LuzBranca_Baixo"):
+    if os.path.isfile(os.path.join("./LuzBranca_Baixo", nome_arquivo)):
+        nome_arquivos.append(nome_arquivo)
 
+for nome_arquivo in nome_arquivos:
+    nome_arquivo.split(".jpg")[0]
+    # programa principal
+    pasta = "LuzBranca_Baixo"
+    arquivo = nome_arquivo.split(".jpg")[0]
 
-diretorioDestino = './imagem_convertida/' + pasta + '/' + arquivo
-if not os.path.exists(diretorioDestino):
-    os.makedirs(diretorioDestino)
+    print("Arquivo: ", nome_arquivo)
 
-# Carrega a imagem
+    nomeDaImagem = nome_arquivo
 
-caminhoDaImagem = ("./LuzBranca_Baixo/" + nomeDaImagem)
+    diretorioDestino = './imagem_convertida/' + pasta + '/' + arquivo
+    if not os.path.exists(diretorioDestino):
+        os.makedirs(diretorioDestino)
 
+    # Carrega a imagem
 
-imagem = cv2.imread(caminhoDaImagem)
-cv2.imwrite("./imagem_convertida/" + pasta + '/' +
-            arquivo+"/01-imagem_original.jpg", imagem)
+    caminhoDaImagem = ("./LuzBranca_Baixo/" + nomeDaImagem)
 
-histograma_original = cv2.calcHist([imagem], [0], None, [256], [0, 256])
-load_histograma(histograma_original, "Histograma Original",
-                "Valores de Pixel", "Frequência")
+    imagem = cv2.imread(caminhoDaImagem)
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+                arquivo+"/01-imagem_original.jpg", imagem)
 
-imagem_to_yuv = cv2.cvtColor(imagem, cv2.COLOR_BGR2YUV)
-imagem_to_yuv[:, :, 0] = cv2.equalizeHist(imagem_to_yuv[:, :, 0])
-imagem_equalizada = cv2.cvtColor(imagem_to_yuv, cv2.COLOR_YUV2BGR)
-cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/01-imagem_equalizada.jpg",
-            imagem_equalizada)
+    histograma_original = cv2.calcHist([imagem], [0], None, [256], [0, 256])
+    load_histograma(histograma_original, "Histograma Original",
+                    "Valores de Pixel", "Frequência")
 
-histograma_equalizado = cv2.calcHist(
-    [imagem_equalizada], [0], None, [256], [0, 256])
+    imagem_to_yuv = cv2.cvtColor(imagem, cv2.COLOR_BGR2YUV)
+    imagem_to_yuv[:, :, 0] = cv2.equalizeHist(imagem_to_yuv[:, :, 0])
+    imagem_equalizada = cv2.cvtColor(imagem_to_yuv, cv2.COLOR_YUV2BGR)
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/01-imagem_equalizada.jpg",
+                imagem_equalizada)
 
-load_histograma(histograma_equalizado, "Histograma Original e Histograma Equalizado",
-                'Valores de Pixel', 'Frequência')
+    histograma_equalizado = cv2.calcHist(
+        [imagem_equalizada], [0], None, [256], [0, 256])
 
-# separa os canais de cores
-blue_channel, green_channel, red_channel = get_rgb_channels(imagem)
+    load_histograma(histograma_equalizado, "Histograma Original e Histograma Equalizado",
+                    'Valores de Pixel', 'Frequência')
 
-cv2.imwrite("./imagem_convertida/" + pasta + '/' +
-            arquivo+"/02-imagem_r.jpg", red_channel)
-cv2.imwrite("./imagem_convertida/" + pasta + '/' +
-            arquivo+"/03-imagem_g.jpg", green_channel)
-cv2.imwrite("./imagem_convertida/" + pasta + '/' +
-            arquivo+"/04-imagem_b.jpg", red_channel)
+    # separa os canais de cores
+    blue_channel, green_channel, red_channel = get_rgb_channels(imagem)
 
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+                arquivo+"/02-imagem_r.jpg", red_channel)
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+                arquivo+"/03-imagem_g.jpg", green_channel)
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+                arquivo+"/04-imagem_b.jpg", red_channel)
 
-imagem_hsv, hue_channel, saturation_channel, value_channel = get_hsv_channels(
-    imagem)
+    imagem_hsv, hue_channel, saturation_channel, value_channel = get_hsv_channels(
+        imagem)
 
-cv2.imwrite("./imagem_convertida/" + pasta + '/' +
-            arquivo+"/05-imagem_hsv.jpg", imagem_hsv)
-cv2.imwrite("./imagem_convertida/" + pasta + '/' +
-            arquivo+"/06-imagem_h.jpg", hue_channel)
-cv2.imwrite("./imagem_convertida/"+pasta + '/' +
-            arquivo +
-            "/07-imagem_s.jpg", saturation_channel)
-cv2.imwrite("./imagem_convertida/" + pasta + '/' +
-            arquivo+"/08-imagem_v.jpg", value_channel)
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+                arquivo+"/05-imagem_hsv.jpg", imagem_hsv)
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+                arquivo+"/06-imagem_h.jpg", hue_channel)
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+                arquivo +
+                "/07-imagem_s.jpg", saturation_channel)
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' +
+                arquivo+"/08-imagem_v.jpg", value_channel)
 
+    # aplicando o filtro
 
-# aplicando o filtro
+    imagem_filtrada = cv2.GaussianBlur(value_channel, (5, 5), 0)
 
-imagem_filtrada = cv2.GaussianBlur(value_channel, (5, 5), 0)
+    cv2.imwrite("./imagem_convertida/"+pasta + '/' +
+                arquivo +
+                "/09-imagem_filtrada.jpg", imagem_filtrada)
 
-cv2.imwrite("./imagem_convertida/"+pasta + '/' +
-            arquivo +
-            "/09-imagem_filtrada.jpg", imagem_filtrada)
+    # limiarização
 
-# limiarização
+    imagem_limiarizada = limiarizar(
+        imagem_filtrada, 127, 255, cv2.THRESH_BINARY)
 
-imagem_limiarizada = limiarizar(imagem_filtrada, 127, 255, cv2.THRESH_BINARY)
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/10-imagem_limiarizada.jpg",
+                imagem_limiarizada)
 
-cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/10-imagem_limiarizada.jpg",
-            imagem_limiarizada)
+    # morfológicas
 
-# morfológicas
+    kernel = np.ones((5, 5), np.uint8)
 
-kernel = np.ones((5, 5), np.uint8)
+    imagem_processada = cv2.morphologyEx(
+        imagem_limiarizada, cv2.MORPH_OPEN, kernel, iterations=4)
+    imagem_processada = cv2.morphologyEx(
+        imagem_processada, cv2.MORPH_CLOSE, kernel, iterations=4)
 
-imagem_processada = cv2.morphologyEx(
-    imagem_limiarizada, cv2.MORPH_OPEN, kernel, iterations=4)
-imagem_processada = cv2.morphologyEx(
-    imagem_processada, cv2.MORPH_CLOSE, kernel, iterations=4)
+    cv2.imwrite("./imagem_convertida/"+pasta + '/' +
+                arquivo +
+                "/11-imagem_dilatada.jpg", imagem_processada)
 
-cv2.imwrite("./imagem_convertida/"+pasta + '/' +
-            arquivo +
-            "/11-imagem_dilatada.jpg", imagem_processada)
+    # multiplicar
 
-# multiplicar
+    imagem_multiplicada = np.multiply(imagem, imagem_processada)
 
-imagem_multiplicada = np.multiply(imagem, imagem_processada)
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/12-imagem_multiplicada.jpg",
+                imagem_multiplicada)
 
-cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/12-imagem_multiplicada.jpg",
-            imagem_multiplicada)
+    # Encontrar contornos
 
+    imagem_cinza = cv2.cvtColor(imagem_multiplicada, cv2.COLOR_BGR2GRAY)
 
-# Encontrar contornos
+    contornos, _ = cv2.findContours(
+        imagem_cinza, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-imagem_cinza = cv2.cvtColor(imagem_multiplicada, cv2.COLOR_BGR2GRAY)
+    # Encontrando o centro de massa
 
-contornos, _ = cv2.findContours(
-    imagem_cinza, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    maior_contorno = max(contornos, key=cv2.contourArea)
+    M = cv2.moments(maior_contorno)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
 
-# Encontrando o centro de massa
+    # Desenhando o centro de massa
+    x = cx - 300 // 2
+    y = cy - 300 // 2
 
-maior_contorno = max(contornos, key=cv2.contourArea)
-M = cv2.moments(maior_contorno)
-cx = int(M['m10']/M['m00'])
-cy = int(M['m01']/M['m00'])
+    retangulo_recortado = imagem[max(y, 0):y + 300, max(x, 0):x + 300]
 
-# Desenhando o centro de massa
-x = cx - 300 // 2
-y = cy - 300 // 2
+    cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/13-retangulo_recortado.jpg",
+                retangulo_recortado)
 
-retangulo_recortado = imagem[max(y, 0):y + 300, max(x, 0):x + 300]
+    variancia = np.var(retangulo_recortado)
+    media = np.mean(retangulo_recortado)
+    desvio_padrao = np.std(retangulo_recortado)
+    mediana = np.median(retangulo_recortado)
 
-cv2.imwrite("./imagem_convertida/" + pasta + '/' + arquivo+"/13-retangulo_recortado.jpg",
-            retangulo_recortado)
-
-variancia = np.var(retangulo_recortado)
-media = np.mean(retangulo_recortado)
-desvio_padrao = np.std(retangulo_recortado)
-mediana = np.median(retangulo_recortado)
-
-print("Variancia: ", variancia)
-print("Media: ", media)
-print("Desvio Padrão: ", desvio_padrao)
-print("Mediana: ", mediana)
+    print("Variancia: ", variancia)
+    print("Media: ", media)
+    print("Desvio Padrão: ", desvio_padrao)
+    print("Mediana: ", mediana)
